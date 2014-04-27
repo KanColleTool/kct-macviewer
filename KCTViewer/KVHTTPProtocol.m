14cd5664d6e03317c91be4da36b4a8128666d201
uppfinnarn <uppfinnarn@gmail.com>
2014-04-27 12:50:50 +0200

    New UA: Recent FF on W7

--- KCTViewer/KVHTTPProtocol.m
+++ KCTViewer/KVHTTPProtocol.m
@@ -25,7 +25,7 @@
 {
 	if((self = [super initWithRequest:[request mutableCopy] cachedResponse:cachedResponse client:client]))
 	{
-		[(NSMutableURLRequest*)self.request setValue:@"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36" forHTTPHeaderField:@"User-Agent"];
+		[(NSMutableURLRequest*)self.request setValue:@"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0" forHTTPHeaderField:@"User-Agent"];
 	}
 	
 	return self;
